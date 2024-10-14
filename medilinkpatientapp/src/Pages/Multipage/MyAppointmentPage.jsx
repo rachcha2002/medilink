9434790caea13b09546b25642d1956505863bec8
Tharindu Eranga <iamtharindunawarathne@gmail.com>
2024-10-14 11:54:13 +0530

    some changes
    add favicon, change my appointment page alignments

--- medilinkpatientapp/src/Pages/Multipage/MyAppointmentPage.jsx
+++ medilinkpatientapp/src/Pages/Multipage/MyAppointmentPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import MultiplePageHeading from '../../Components/Hero/MultiplePageHeading';
 import axios from 'axios';
 import { useAuthContext } from "../../Context/AuthContext";
-import { Table, Button, Modal, Row, Col } from 'react-bootstrap';
+import { Table, Button, Modal, Row, Col, Form } from 'react-bootstrap';
 
 import PayOnlineCommand from "../../Components/Payments/PayOnlineCommand"
 
@@ -21,9 +21,8 @@ const MyAppointmentPage = () => {
   const [selectedClinicAppointment, setSelectedClinicAppointment] = useState(null);
   const [selectedScanAppointment, setSelectedScanAppointment] = useState(null);
   const [selectedStatus, setSelectedStatus] = useState('all');
-
-
-  
+  const [editableDate, setEditableDate] = useState('');
+  const [editableTime, setEditableTime] = useState('');
 
   useEffect(() => {
     if (!userid) return; // Exit if userid is not available
@@ -35,13 +34,17 @@ const MyAppointmentPage = () => {
 
         // Ensure response data has the expected properties
         const clinicandchannelingAppointments = [
-          ...(response.data.clinicappointments || []),
-          ...(response.data.channelingappointments || []),
+          ...(response.data.clinicappointments || []).map(appointment => ({ ...appointment, type: 'clinic' })),
+          ...(response.data.channelingappointments || []).map(appointment => ({ ...appointment, type: 'channeling' })),
         ];
-        const scanappointments = [...(response.data.testscanappointments || [])];
+        const scanappointments = [
+          ...(response.data.testscanappointments || []).map(appointment => ({ ...appointment, type: 'testscan' }))
+        ];
 
         setAppointments(clinicandchannelingAppointments);
         settestsandscans(scanappointments); 
+        console.log('Appointments:', clinicandchannelingAppointments);
+        console.log('Tests and Scans:', scanappointments);
       } catch (error) {
         console.error('Error fetching appointments:', error);
       }
@@ -49,8 +52,6 @@ const MyAppointmentPage = () => {
 
     fetchAppointments();
   }, [userid]);
-
-
 
   const PayForAppointment = async (selectedAppointment) => {
     try {
@@ -80,12 +81,16 @@ const MyAppointmentPage = () => {
   const handleCloseClinicModal = () => setShowClinicModal(false);
   const handleShowClinicModal = (appointment) => {
     setSelectedClinicAppointment(appointment);
+    setEditableDate(appointment.appointmentDate);
+    setEditableTime(appointment.appointmentTime);
     setShowClinicModal(true);
   };
 
   const handleCloseScanModal = () => setShowScanModal(false);
   const handleShowScanModal = (appointment) => {
     setSelectedScanAppointment(appointment);
+    setEditableDate(appointment.appointmentDate);
+    setEditableTime(appointment.appointmentTime);
     setShowScanModal(true);
   };
 
@@ -95,6 +100,36 @@ const MyAppointmentPage = () => {
       const statusMatch = selectedStatus === 'all' || appointmentStatus === selectedStatus.toLowerCase();
       return statusMatch;
     });
+  };
+
+  const handleReschedule = async (appointment) => {
+    try {
+      const updatedAppointment = {
+        ...appointment,
+        appointmentDate: editableDate,
+        appointmentTime: editableTime,
+      };
+
+      await axios.put(`${process.env.REACT_APP_BACKEND_URL}/api/appointment/updateappointment/${appointment.type}/${appointment._id}`, updatedAppointment);
+      setAppointments(prevAppointments => prevAppointments.map(appt => appt._id === appointment._id ? updatedAppointment : appt));
+      settestsandscans(prevTestsAndScans =>prevTestsAndScans.map(appt => appt._id === appointment._id ? updatedAppointment : appt));
+      setShowClinicModal(false);
+      setShowScanModal(false);
+    } catch (error) {
+      console.error('Error rescheduling appointment:', error);
+    }
+  };
+
+  const handleDelete = async (appointment) => {
+    try {
+      await axios.delete(`${process.env.REACT_APP_BACKEND_URL}/api/appointment/deleteappointment/${appointment.type}/${appointment._id}`);
+      setAppointments(prevAppointments => prevAppointments.filter(appt => appt._id !== appointment._id));
+      settestsandscans(prevTestsAndScans => prevTestsAndScans.filter(appt => appt._id !== appointment._id));
+      setShowClinicModal(false);
+      setShowScanModal(false);
+    } catch (error) {
+      console.error('Error deleting appointment:', error);
+    }
   };
 
   // Filter both clinic/channeling and scan/test appointments
@@ -109,7 +144,7 @@ const MyAppointmentPage = () => {
           {['all', 'pending', 'rejected', 'approved', 'completed'].map(status => (
             <Button
               key={status}
-              className={`me-3 st-btn st-style1 st-color1 st-size-medium ${selectedStatus === status ? 'active' : ''}`}
+              className={`me-3 mb-2 st-btn st-style1 st-color1 st-size-medium ${selectedStatus === status ? 'active' : ''}`}
               onClick={() => setSelectedStatus(status)}
             >
               {status.charAt(0).toUpperCase() + status.slice(1)}
@@ -139,9 +174,9 @@ const MyAppointmentPage = () => {
                 <td>{new Date(appointment.appointmentDate).toLocaleDateString()}</td>
                 <td>{appointment.appointmentTime}</td>
                 <td>
-                  <Button className="st-btn st-style1 st-color1 st-size-medium" onClick={() => handleShowClinicModal(appointment)}>View</Button>
+                  <Button className="st-btn st-style1 st-color1 st-size-medium me-2 mb-2" onClick={() => { handleShowClinicModal(appointment); setEditableDate(appointment.appointmentDate.split('T')[0]); }}>View</Button>
                   {appointment.status === 'approved' && (
-                     <Button variant="success" onClick={()=>PayForAppointment(appointment)}>Pay</Button>
+                     <Button variant="success" size="lg" className="mb-2" onClick={()=>PayForAppointment(appointment)}>Pay</Button>
                   )}
                 </td>
               </tr>
@@ -172,9 +207,9 @@ const MyAppointmentPage = () => {
                 <td>{new Date(appointment.appointmentDate).toLocaleDateString()}</td>
                 <td>{appointment.appointmentTime}</td>
                 <td>
-                  <Button className="st-btn st-style1 st-color1 st-size-medium" onClick={() => handleShowScanModal(appointment)}>View</Button>
+                  <Button className="st-btn st-style1 st-color1 st-size-medium me-2 mb-2" onClick={() => { handleShowScanModal(appointment); setEditableDate(appointment.appointmentDate.split('T')[0]); }}>View</Button>
                   {appointment.status === 'approved' && (
-                     <Button variant="success" onClick={()=>PayForAppointment(appointment)}>Pay</Button>
+                     <Button variant="success" size="lg" className="mb-2" onClick={()=>PayForAppointment(appointment)}>Pay</Button>
                   )}
                 </td>
               </tr>
@@ -190,14 +225,50 @@ const MyAppointmentPage = () => {
         <Modal.Body>
           {selectedClinicAppointment && (
             <>
-              <p><strong>Appointment Date:</strong> {new Date(selectedClinicAppointment.appointmentDate).toLocaleDateString()}</p>
-              <p><strong>Appointment Time:</strong> {selectedClinicAppointment.appointmentTime}</p>
-              <p><strong>Doctor Name:</strong> {selectedClinicAppointment.doctorName}</p>
-              {(selectedClinicAppointment.status === 'approved' || selectedClinicAppointment.status === 'completed') && (
+              {selectedClinicAppointment.status === 'pending' ? (
                 <>
-                  <p><strong>Payment:</strong> Rs.{selectedClinicAppointment.payment}</p>
-                  {selectedClinicAppointment.status === 'approved' && (
-                     <Button variant="success" onClick={()=>PayForAppointment(selectedClinicAppointment)}>Pay</Button>
+                <p><strong>Hospital:</strong>{selectedClinicAppointment.hospitalName}</p>
+                <p><strong>Doctor Name:</strong> {selectedClinicAppointment.doctorName}</p>
+                  <Form.Group controlId="formAppointmentDate">
+                    <Form.Label>Appointment Date</Form.Label>
+                    <Form.Control
+                      type="date"
+                      value={editableDate}
+                      onChange={(e) => setEditableDate(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="formAppointmentTime">
+                    <Form.Label>Appointment Time</Form.Label>
+                    <Form.Control
+                      type="time"
+                      value={editableTime}
+                      onChange={(e) => setEditableTime(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Button className="mt-2 st-btn st-style1 st-color1 st-size-medium me-2" onClick={() => handleReschedule(selectedClinicAppointment)}>Reschedule</Button>
+                  <Button variant="danger" size="lg" className="mt-2" onClick={() => handleDelete(selectedClinicAppointment)}>Delete</Button>
+                </>
+              ) : selectedClinicAppointment.status === 'rejected' ? (
+                <>
+                  <p><strong>Appointment Date:</strong> {new Date(selectedClinicAppointment.appointmentDate).toLocaleDateString()}</p>
+                  <p><strong>Appointment Time:</strong> {selectedClinicAppointment.appointmentTime}</p>
+                  <p><strong>Hospital:</strong>{selectedClinicAppointment.hospitalName}</p>
+                  <p><strong>Doctor Name:</strong> {selectedClinicAppointment.doctorName}</p>
+                  <Button variant="danger" size="lg" onClick={() => handleDelete(selectedClinicAppointment)}>Delete</Button>
+                </>
+              ) : (
+                <>
+                  <p><strong>Appointment Date:</strong> {new Date(selectedClinicAppointment.appointmentDate).toLocaleDateString()}</p>
+                  <p><strong>Appointment Time:</strong> {selectedClinicAppointment.appointmentTime}</p>
+                  <p><strong>Hospital:</strong>{selectedClinicAppointment.hospitalName}</p>
+                  <p><strong>Doctor Name:</strong> {selectedClinicAppointment.doctorName}</p>
+                  {(selectedClinicAppointment.status === 'approved' || selectedClinicAppointment.status === 'completed') && (
+                    <>
+                      <p><strong>Payment:</strong> Rs.{selectedClinicAppointment.payment}</p>
+                      {selectedClinicAppointment.status === 'approved' && (
+                        <Button variant="success" size="lg" onClick={()=>PayForAppointment(selectedClinicAppointment)}>Pay</Button>
+                      )}
+                    </>
                   )}
                 </>
               )}
@@ -218,14 +289,50 @@ const MyAppointmentPage = () => {
         <Modal.Body>
           {selectedScanAppointment && (
             <>
-              <p><strong>Appointment Date:</strong> {new Date(selectedScanAppointment.appointmentDate).toLocaleDateString()}</p>
-              <p><strong>Appointment Time:</strong> {selectedScanAppointment.appointmentTime}</p>
-              <p><strong>Scan Name:</strong> {selectedScanAppointment.scanName}</p>
-              {(selectedScanAppointment.status === 'approved' || selectedScanAppointment.status === 'completed') && (
+              {selectedScanAppointment.status === 'pending' ? (
                 <>
-                  <p><strong>Payment:</strong> Rs.{selectedScanAppointment.payment}</p>
-                  {selectedScanAppointment.status === 'approved' && (
-                    <Button variant="success" onClick={()=>PayForAppointment(selectedScanAppointment)}>Pay</Button>
+                <p><strong>Hospital:</strong>{selectedScanAppointment.hospitalName}</p>
+                <p><strong>Scan Name:</strong> {selectedScanAppointment.scanName}</p>
+                  <Form.Group controlId="formAppointmentDate">
+                    <Form.Label>Appointment Date</Form.Label>
+                    <Form.Control
+                      type="date"
+                      value={editableDate}
+                      onChange={(e) => setEditableDate(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="formAppointmentTime">
+                    <Form.Label>Appointment Time</Form.Label>
+                    <Form.Control
+                      type="time"
+                      value={editableTime}
+                      onChange={(e) => setEditableTime(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Button className="mt-2 st-btn st-style1 st-color1 st-size-medium me-2" onClick={() => handleReschedule(selectedScanAppointment)}>Reschedule</Button>
+                  <Button variant="danger" size="lg" className="mt-2" onClick={() => handleDelete(selectedScanAppointment)}>Delete</Button>
+                </>
+              ) : selectedScanAppointment.status === 'rejected' ? (
+                <>
+                   <p><strong>Appointment Date:</strong> {new Date(selectedScanAppointment.appointmentDate).toLocaleDateString()}</p>
+                   <p><strong>Appointment Time:</strong> {selectedScanAppointment.appointmentTime}</p>
+                   <p><strong>Hospital:</strong>{selectedScanAppointment.hospitalName}</p>
+                  <p><strong>Scan Name:</strong> {selectedScanAppointment.scanName}</p>
+                  <Button variant="danger" size="lg" onClick={() => handleDelete(selectedScanAppointment)}>Delete</Button>
+                </>
+              ) : (
+                <>
+                  <p><strong>Appointment Date:</strong> {new Date(selectedScanAppointment.appointmentDate).toLocaleDateString()}</p>
+                  <p><strong>Appointment Time:</strong> {selectedScanAppointment.appointmentTime}</p>
+                  <p><strong>Hospital:</strong>{selectedScanAppointment.hospitalName}</p>
+                  <p><strong>Scan Name:</strong> {selectedScanAppointment.scanName}</p>
+                  {(selectedScanAppointment.status === 'approved' || selectedScanAppointment.status === 'completed') && (
+                    <>
+                      <p><strong>Payment:</strong> Rs.{selectedScanAppointment.payment}</p>
+                      {selectedScanAppointment.status === 'approved' && (
+                        <Button variant="success" size="lg" onClick={()=>PayForAppointment(selectedScanAppointment)}>Pay</Button>
+                      )}
+                    </>
                   )}
                 </>
               )}
